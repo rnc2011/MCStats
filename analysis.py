4a36871ab88caa1023a1a11441d7f5a0a844433f
Richard <rncovington0455@gmail.com>
2020-02-18 17:37:04 -0500

    Plot annual logins

--- analysis.py
+++ analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Read in the data
 df = pd.read_csv("logins-2016-2020.csv")
@@ -8,12 +9,16 @@ var_names = ["Logins 2016", "Logins 2017", "Logins 2018", "Logins 2019", "Logins
 df = pd.melt(df, id_vars = ["Date"], value_vars = var_names)
 df = df.dropna()
 
-# Get just the year
-df['variable'] = df['variable'].str.replace(r'\D', '')
-# Convert year to integer
+# Drop "Logins " and keep just the year
+df['year'] = df['variable'].str.replace(r'\D', '')
 
 # Plot the annual logins
+annual_logins = df.groupby(["year"]).sum()
+plt.plot(annual_logins["value"])
+plt.show()
 
+# Convert year to integer
+# df["year"] = pd.to_numeric(df['year'])
 
 # Separate date column into month and day
 
